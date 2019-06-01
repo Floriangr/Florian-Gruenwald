7a47cd4ba3adaa6d2389ddaff562a29f6664e7f1
Florian <florian.gruenwald@hec.edu>
2019-06-01 14:06:43 +0200

    Style header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="image">
-    <img src="../assets/header.jpg" alt="">
-    <h1>Welcome to my world!</h1>
+    <img src="../assets/header.jpg" alt="" />
+    <h1>Florian Gruenwald</h1>
+    <h2><i>Product & team builder</i></h2>
   </div>
 </template>
 
@@ -15,15 +16,24 @@ export default {};
 }
 
 .image img {
-  width:100%;
+  width: 100%;
   display: block;
   max-height: 20vw;
 }
 
 .image h1 {
-  font-size: 6vw;
+  font-size: 4vw;
   position: absolute;
   top: 10%;
+  left: 10%;
+  color: orange;
+}
+
+.image h2 {
+  font-size: 2vw;
+  position: absolute;
+  top: 40%;
+  left: 10%;
   color: orange;
 }
 </style>
