37b51c9efe7c19eac46ac909d234788e91cd0fce
Florian <florian.gruenwald@hec.edu>
2019-06-04 19:21:10 +0200

    add comps to contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <TheHeader />
+    <TheNavBar />
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSevlAJ0qNIfuDSpf23CT81H0SFXXtKbLxEfL0HrR-_SDHbNHw/viewform?embedded=true"
+      width="640"
+      height="1019"
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+    >
+      Loading...
+    </iframe>
+    <TheFooter />
   </div>
 </template>
+
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+import TheNavBar from "@/components/TheNavBar.vue";
+import TheFooter from "@/components/TheFooter.vue";
+
+export default {
+  name: "home",
+  components: {
+    TheHeader,
+    TheNavBar,
+    TheFooter
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
